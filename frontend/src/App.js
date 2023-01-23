884f4bcc55cd2721e5d40dbe91df149aa2d82dfe
jjf02987 <jjf02987@gmail.com>
2023-01-23 11:21:26 -0500

    Video3-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="/">Glass Gone Wow McKnight</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
