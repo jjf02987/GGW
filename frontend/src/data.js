7794f7efb10c5aa85f2a95156ccf71c3790853f6
jjf02987 <jjf02987@gmail.com>
2023-01-23 11:47:17 -0500

    Video5-React-App-Data

---
+++ frontend/src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    products:  [
+        {
+            name: "product 1",
+            slug: "product-1",
+            category: "products",
+            imageP: "/images/p1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: "MJ",
+            rating: 4.5,
+            numReviews: 10,
+            description: "This product really products."
+        },
+    ],
+};
