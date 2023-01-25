b511d9a73ccff5ccc086ce2ca3ed1168451954fe
jjf02987 <jjf02987@gmail.com>
2023-01-25 10:39:56 -0500

    data.js V5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,14 @@ function App() {
       <a href="/">Glass Gone Wow McKnight</a>
       </header>
       <main>
-        List Products
+        <h1> Featured Glass </h1>
+        {
+          data.products.map(product => (<div>
+            <img src= {product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+          ))}
       </main>
     </div>
   );
