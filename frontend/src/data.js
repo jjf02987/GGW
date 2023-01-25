0cf8fbc47236f59b29425a1a517f79e6da741209
jjf02987 <jjf02987@gmail.com>
2023-01-25 10:37:09 -0500

    dtat.js V5

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -4,7 +4,7 @@ const data = {
             name: "10\" Glass Marijuana Print Water Pipe",
             slug: "product-1",
             category: "products",
-            image: "C:/GGW/frontend/public/images/glass-bong-ganja.webp",
+            image: "C:/GGW/frontend/public/images/glass-bong-ganja.webp", // 679px x 829px
             price: 120,
             countInStock: 10,
             brand: "MJ",
@@ -16,7 +16,7 @@ const data = {
             name: "Fumed 10\" Silver mini Witch Doctor",
             slug: "product-2",
             category: "products",
-            image: "C:/GGW/frontend/public/images/Fumed 10in Silver mini Witch Doctor.jpg",
+            image: "C:/GGW/frontend/public/images/Fumed 10in Silver mini Witch Doctor.jpg", // 679px x 829px
             price: 120,
             countInStock: 10,
             brand: "MJ",
@@ -28,7 +28,7 @@ const data = {
             name: "8\" silcon knot with recycler",
             slug: "product-3",
             category: "products",
-            image: "C:/GGW/frontend/public/images/glass-bong-glass-water-pipe.jpg",
+            image: "C:/GGW/frontend/public/images/glass-bong-glass-water-pipe.jpg", // 679px x 829px
             price: 120,
             countInStock: 10,
             brand: "MJ",
@@ -40,7 +40,7 @@ const data = {
             name: "10\" Lookah Heady",
             slug: "product-4",
             category: "products",
-            image: "C:/GGW/frontend/public/images/LookahHeady.jpg",
+            image: "C:/GGW/frontend/public/images/LookahHeady.jpg", // 679px x 829px
             price: 120,
             countInStock: 10,
             brand: "MJ",
