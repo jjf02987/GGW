ea7267db22158fdbd9c042a6c5a5dd63937e4112
jjf02987 <jjf02987@gmail.com>
2023-01-27 14:03:33 -0500

    update 10

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products:  [
+        {
+            name: "10\" Glass Marijuana Print Water Pipe",
+            slug: "10\" Glass Marijuana Print Water Pipe",
+            category: "products",
+            image: "images/glass-bong-ganja.webp", // 679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: "MJ",
+            rating: 4.5,
+            numReviews: 10,
+            description: "This product really products."
+        },
+        {
+            name: "Fumed 10\" Silver mini Witch Doctor",
+            slug: "Fumed 10\" Silver mini Witch Doctor",
+            category: "products",
+            image: "images/Fumed 10in Silver mini Witch Doctor.jpg", // 679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: "MJ",
+            rating: 4.5,
+            numReviews: 10,
+            description: "This product really products."
+        },
+        {
+            name: "8\" silcon knot with recycler",
+            slug: "8\" silcon knot with recycler",
+            category: "products",
+            image: "images/glass-bong-glass-water-pipe.jpg", // 679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: "MJ",
+            rating: 4.5,
+            numReviews: 10,
+            description: "This product really products."
+        },
+        {
+            name: "10\" Lookah Heady",
+            slug: "10\" Lookah Heady",
+            category: "products",
+            image: "images/LookahHeady.jpg", // 679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: "MJ",
+            rating: 4.5,
+            numReviews: 10,
+            description: "This product really products."
+        },
+    ],
+};
+export default data;
