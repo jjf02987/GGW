82df6113521b441910126daed8cfbacb74f3b615
jjf02987 <jjf02987@gmail.com>
2023-01-31 14:07:26 -0500

    update 12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-        <a href="/">Glass Gone Wow McKnight</a>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-      </main>
-    </div>
+      <div>
+        <header>
+          <Link to="/">Glass Gone Wow McKnight</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
