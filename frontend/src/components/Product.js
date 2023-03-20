85fc542b977714abf4ea059f14b849245a066b91
jjf02987 <jjf02987@gmail.com>
2023-03-20 16:44:12 -0400

    update 59

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import Rating from "../Rating";
+import Rating from "./Rating";
 import axios from "axios";
 import { useContext } from "react";
 import { Store } from "../Store";
@@ -27,12 +27,13 @@ function Product(props) {
       payload: { ...item, quantity },
     });
   };
+
   return (
-    <Card className="card">
+    <Card>
       <Link to={`/product/${product.slug}`}>
         <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
-      <Card.Body className="card-body">
+      <Card.Body>
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
