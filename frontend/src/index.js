af680ae1b01b8feac72b80b2ab15368a589f6b33
Jason Fahrenhold <72748366+jjf02987@users.noreply.github.com>
2023-03-21 11:09:31 -0400

    Update index.js
    Changed App import to app. Case sensitive error in build.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import { HelmetProvider } from "react-helmet-async";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "./App";
+import app from "./app";
 import reportWebVitals from "./reportWebVitals";
 import { StoreProvider } from "./Store";
 
@@ -14,7 +14,7 @@ root.render(
     <StoreProvider>
       <HelmetProvider>
         <PayPalScriptProvider deferLoading={true}>
-          <App />
+          <app />
         </PayPalScriptProvider>
       </HelmetProvider>
     </StoreProvider>
